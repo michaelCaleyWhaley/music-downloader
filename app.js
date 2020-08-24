dc7cdfdad6572d8f5db7483428bdb6a83392822c
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2020-08-24 22:09:56 +0100

    convert script to class

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ import fetch from "node-fetch";
 import SpotifyWebApi from "spotify-web-api-node";
 
 class SpotifyTrackFinder {
-  constructor({ clientId, clientSecret, playlistName = "" }) {
+  constructor({
+    clientId,
+    clientSecret,
+    playlistName = "2021",
+    userName = "sircaley",
+  }) {
     this.spotifyApi = new SpotifyWebApi({
       clientId: clientId,
       clientSecret: clientSecret,
     });
     this.playListName = playlistName;
+    this.userName = userName;
   }
 
   init = () => {
@@ -19,32 +25,46 @@ class SpotifyTrackFinder {
       const data = await this.spotifyApi.clientCredentialsGrant();
       global.accessToken = data.body["access_token"];
       this.spotifyApi.setAccessToken(data.body["access_token"]);
-      this.getUserData();
+      this.getTrackHref();
     } catch (e) {
-      console.log(`e: `, e);
+      console.log(`spotifyAuthentication error: `, e);
     }
   };
 
-  getUserData = async () => {
-    const data = await this.spotifyApi.getUserPlaylists("sircaley");
-
-    const playlists = data.body.items.filter((item) => {
-      return item.name === "2021";
+  findPlaylist = (playlistArray) => {
+    return playlistArray.filter((item) => {
+      return item.name === this.playListName;
     });
-    if (playlists.length < 1) {
-      console.log(`LOG: less than 1 playlist found`);
-      return;
+  };
+
+  getTrackHref = async () => {
+    try {
+      const data = await this.spotifyApi.getUserPlaylists(this.userName);
+
+      const playlists = this.findPlaylist(data.body.items);
+      if (playlists.length < 1) {
+        console.log(`LOG: less than 1 playlist found`);
+        return;
+      }
+      const [
+        {
+          tracks: { href },
+        },
+      ] = playlists;
+      this.fetchTrackData(href);
+    } catch (e) {
+      console.log(`getUserData error: `, e);
     }
+  };
 
-    const [
-      {
-        tracks: { href },
-      },
-    ] = playlists;
-console.log(`playlists: `, playlists);
+  fetchTrackData = async (url) => {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: { Authorization: "Bearer " + global.accessToken },
+    });
+    const json = await response.json();
 
-    // const trackData = await getData(href);
-    // console.log(`trackData: `, trackData.items[0].track);
+    console.log(`json: `, json);
 
     // console.log(`track name: `, trackData.items[0].track.name);
     // console.log(`artists: `, trackData.items[0].track.artists[0].name);
@@ -55,62 +75,3 @@ const clientId = process.env.spotifyClientId;
 const clientSecret = process.env.spotifyClientSecret;
 const spotifyTrackFinder = new SpotifyTrackFinder({ clientId, clientSecret });
 spotifyTrackFinder.init();
-
-// Create the api object with the credentials
-// const spotifyApi = new SpotifyWebApi({
-//   clientId: clientId,
-//   clientSecret: clientSecret,
-// });
-
-// Retrieve an access token.
-// spotifyApi.clientCredentialsGrant().then(
-//   function (data) {
-//     console.log("The access token expires in " + data.body["expires_in"]);
-//     console.log("The access token is " + data.body["access_token"]);
-
-//     // Save the access token so that it's used in future calls
-//     global.accessToken = data.body["access_token"];
-//     spotifyApi.setAccessToken(data.body["access_token"]);
-//     getUserPlaylist();
-//   },
-//   function (err) {
-//     console.log("Something went wrong when retrieving an access token", err);
-//   }
-// );
-
-// const getUserPlaylist = () => {
-//   spotifyApi.getUserPlaylists("sircaley").then(
-//     async function (data) {
-//       const playlists = data.body.items.filter((item) => {
-//         return item.name === "2021";
-//       });
-//       if (playlists.length < 1) {
-//         console.log(`LOG: less than 1 playlist found`);
-//         return;
-//       }
-
-//       const [
-//         {
-//           tracks: { href },
-//         },
-//       ] = playlists;
-
-//       const trackData = await getData(href);
-//       // console.log(`trackData: `, trackData.items[0].track);
-
-//       console.log(`track name: `, trackData.items[0].track.name);
-//       console.log(`artists: `, trackData.items[0].track.artists[0].name);
-//     },
-//     function (err) {
-//       console.log("Something went wrong!", err);
-//     }
-//   );
-// };
-
-async function getData(url) {
-  const response = await fetch(url, {
-    method: "GET",
-    headers: { Authorization: "Bearer " + global.accessToken },
-  });
-  return response.json();
-}
